72df73786ddb956c32df031bc9b190792676f182
dmitry <dmitriy4ggl@gmail.com>
2020-02-12 18:10:07 +0300

    9th tests

--- 10/src/test/testing.cpp
+++ 10/src/test/testing.cpp
@@ -13,9 +13,39 @@ TEST(rotate, wrong_number_of_size) {
     EXPECT_FALSE(rotate(vec, 3.3));
 }
 
-TEST(rotate, correct_answer){
+
+TEST(rotate, correct_answer_0) {
+    std::vector<int> vec = {3, 4};
+    rotate(vec, 4);
+    EXPECT_EQ(3, vec[0]);
+    EXPECT_EQ(4, vec[1]);
+}
+
+TEST(rotate, correct_answer_1) {
+    std::vector<int> vec = {3, 4};
+    rotate(vec, 1);
+    EXPECT_EQ(-4, vec[0]);
+    EXPECT_EQ(3, vec[1]);
+}
+
+TEST(rotate, correct_answer_2) {
     std::vector<int> vec = {3, 4};
     rotate(vec, 2);
+    EXPECT_EQ(-3, vec[0]);
+    EXPECT_EQ(-4, vec[1]);
+}
+
+TEST(rotate, correct_answer_3) {
+    std::vector<int> vec = {3, 4};
+    rotate(vec, 3);
+    EXPECT_EQ(4, vec[0]);
+    EXPECT_EQ(-3, vec[1]);
+}
+
+
+TEST(rotate, correct_answer_more_than_3) {
+    std::vector<int> vec = {3, 4};
+    rotate(vec, 10);
     EXPECT_EQ(-3, vec[0]);
     EXPECT_EQ(-4, vec[1]);
 }
